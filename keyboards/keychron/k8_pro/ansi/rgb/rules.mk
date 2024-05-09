25d9cddd74bddeab75bc7c38a0542bc4f49c7976
zura <139991261+riel-01@users.noreply.github.com>
2024-05-09 18:51:59 +0800

    Update rules.mk

--- keyboards/keychron/k8_pro/ansi/rgb/rules.mk
+++ keyboards/keychron/k8_pro/ansi/rgb/rules.mk
@@ -1,1 +1,7 @@
-# This file intentionally blank
+OPENRGB_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+MOUSEKEY_ENABLE = no
+
+ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
+    SRC += rgb_matrix_user.c
+endif
